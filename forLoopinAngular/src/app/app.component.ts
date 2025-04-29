db617c378925e649f63af6ebf63b6535f82e12c2
Sha1Pokala <shashankpokala99@gmail.com>
2025-04-28 20:42:23 -0400

    For loop in Angular

--- forLoopinAngular/src/app/app.component.ts
+++ forLoopinAngular/src/app/app.component.ts
@@ -3,13 +3,24 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'forLoopinAngular';
 
-  user = ["shashank", "likhita", "Peter", "Bruce", "Tony"]
 
+  users = ["shashank", "likhita", "Peter", "Bruce", "Tony"];
+
+  students = [
+    { fullName: 'Shashank Pokala', age: 25, email: 'shashank@Test.com' },
+    { fullName: 'Likhita Pulijala', age: 26, email: 'likhita@Test.com' },
+    { fullName: 'Sam gupta', age: 27, email: 'Sam@Test.com' },
+    { fullName: 'Pandu reddy', age: 30, email: 'Pandu@Test.com' },
+  ]
+
+  getName(name: string) {
+    console.log(name);
+  }
 }
