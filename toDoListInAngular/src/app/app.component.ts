ac45843dbb0ff915e0326ce50bbbce3ed7f4bbda
Sha1Pokala <shashankpokala99@gmail.com>
2025-04-29 17:30:59 -0400

    To Do List Using Angular

---
+++ toDoListInAngular/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+  title = 'toDoListInAngular';
+
+  task = ' ';
+  taskList: { id: number; task: string }[] = [];
+
+  addTask() {
+    this.taskList.push({ id: this.taskList.length + 1, task: this.task });
+    console.log(this.taskList);
+    this.task = '';
+  }
+  deleteTask(taskID: number) {
+    this.taskList = this.taskList.filter((item) => item.id != taskID)
+
+  }
+
+
+
+}
